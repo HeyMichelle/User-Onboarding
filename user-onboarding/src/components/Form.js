c1ed0ac85bc99af0bda5607ebffe41e7983e1bb3
Michelle Hey <miscmiche@gmail.com>
2020-05-14 20:31:16 -0400

    Form completed but submit still requires onChange handling.

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,10 +1,21 @@
 import React, {useState} from 'react';
 import styled from 'styled-components';
 
+
+
+// questions for TL
+// Mention formik prev. lessions vs. difference in this one
+
+
 export default function Form() {
     // form input state-management
     const [formState, setFormState] = useState({
-        name: ""
+        name: "",
+        email: "",
+        password: "",
+        questions: "",
+        dropdown: "",
+        terms: false  
     });
 
     // onSubmit function
@@ -14,9 +25,11 @@ export default function Form() {
     }
 
     // onChange function
-    const inputChange = e => {
+    const inputChanges = e => {
         console.log("input changed!", e.target.value);
-        setFormState({ name: e.target.value });
+        // for check boxes, apply below, not good for multiple selects
+        let value = e.target.type === "checkbox" ? e.target.checked : e.target.value;
+        setFormState({ ...formState, [e.target.name]: e.target.value });
     }
 
 
@@ -26,35 +39,91 @@ export default function Form() {
         width: 80%;
         max-width: 300px;
         margin: 0 auto;
+        padding: 5%;
     `;
-
+ 
     return (
-        <FormWrapper> 
-            <form onSubmit={formSubmit}>
-                <label htmlFor="name"> 
-                    Name 
-                    <input type="text" name="name" id="name" placeholder="Name"/>
-                </label><br />
+        <div className="container">
+            Sign Up Email/Password Form
+            <FormWrapper> 
+                <form onSubmit={formSubmit}>
+                    <label htmlFor="name"> 
+                        Name:  
+                        <input 
+                            type="text" 
+                            name="name" 
+                            id="name" 
+                            placeholder="Name"
+                            value={formState.name}
+                            onChange={inputChanges} 
+                        />
+                    </label><br />
 
-                <label htmlFor="email"> 
-                    Email 
-                    <input type="text" name="email" id="email" placeholder="Email"/>
-                </label><br />
+                    <label htmlFor="email"> 
+                        Email:  
+                        <input 
+                            type="text" 
+                            name="email" 
+                            id="email" 
+                            placeholder="Email" 
+                            value={formState.email} 
+                            onChange={inputChanges}
+                        />
+                    </label><br />
 
-                <label htmlFor="password"> 
-                    Password 
-                    <input type="text" name="password" id="password" placeholder="Password"/>
-                </label><br />
-                <label htmlFor="ToS"> 
-                    Terms of Service 
-                    <input type="checkbox" name="ToS" />
-                </label>
+                    <label htmlFor="password"> 
+                        Password:  
+                        <input 
+                            type="text" 
+                            name="password" 
+                            id="password" 
+                            placeholder="Password"
+                            value={formState.password} 
+                            onChange={inputChanges}
+                        />
+                    </label><br /><br />
 
-                {/* <input type="submit" value="Submit"></input> */}
-                <button>Submit</button>
+                    <label htmlFor="questions">
+                        Questions, comments, concerns?
+                        <textarea 
+                            name="questions"
+                            id=""
+                            placeholder="Hit enter for multiple lines..."
+                            onChange={inputChanges}
+                        />
+                    </label><br/>
 
-            </form>
-        </FormWrapper>
+                    <label htmlFor="dropdown">
+                        Here is a dropdown <br />
+                        <select 
+                            value={formState.dropdown} 
+                            name="dropdown" 
+                            id="dropdown" 
+                            onChange={inputChanges}
+                            >
+                                <option value="optionOne">Blue pill</option>
+                                <option value="optionTwo">Red pill</option>
+                        </select>
+                    </label><br /> <br />
+
+                    <label htmlFor="terms">
+                        I agree to the Terms & Conditions
+                        <input 
+                            type="checkbox" 
+                            name="terms"
+                            id="terms"
+                            checked={formState.value}
+                            onChange={inputChanges} 
+                                {/* terinary operator used for checkboxes in input change logic*/}
+                        />
+                    </label><br /><br />
+
+                    {/* <input type="submit" value="Submit"></input> */}
+                    <button>Submit</button>
+
+                </form>
+            </FormWrapper>
+        </div>
     );
 }
 
